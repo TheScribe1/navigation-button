df48e81d6cf99fc0f5cfffaf7bb058269a5b1140
BlackBox11 <sumonahmed5841@gmail.com>
2025-04-30 16:10:27 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/navigation_button/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigation_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/converter_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+class ConverterPage extends StatefulWidget {
+  const ConverterPage({super.key});
+  @override
+  State<ConverterPage> createState() => _ConverterPageState();
+}
+
+class _ConverterPageState extends State<ConverterPage> {
+
+  double result = 0;
+  TextEditingController textEditingController = TextEditingController();
+
+  void convert() {
+    final text = textEditingController.value.text;
+    if (text.isEmpty) {
+      errorText = 'Can\'t be empty';
+    } else {
+      errorText = null;
+      result = double.parse(textEditingController.text) * 122.23;
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  String? errorText;
+
+  @override
+  Widget build(Object context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.red,
+        width: 2,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(40)),
+    );
+
+    final textStyle = TextStyle(
+      color: Colors.yellow,
+      fontSize: 23,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(
+            color: Colors.yellow,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 5,
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          height: 500,
+          child: Card(
+            elevation: 5,
+            // shadowColor: const Color.fromARGB(255, 104, 95, 95),
+            // color: const Color.fromARGB(255, 255, 255, 255),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('USD ', style: textStyle),
+                      Icon(Icons.arrow_forward, color: Colors.yellow),
+                      Text(' BDT', style: textStyle),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: textEditingController,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      errorText: errorText,
+                      errorBorder: border,
+                      focusedErrorBorder: border,
+                      label: Text("Amount"),
+                      labelStyle: TextStyle(color: Colors.white),
+                      hintText: "Enter amount",
+                      prefixIcon: Icon(Icons.monetization_on),
+                      prefixIconColor: Colors.green,
+                      focusedBorder: border,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: true,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () => convert(),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    child: Text("Convert"),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'BDT ${result.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
